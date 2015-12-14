57f9938cb7ad924ca481ed08afa4daddff605472
NathanGrimaud <grimaudnathan@gmail.com>
2015-12-14 17:49:08 +0100

    Ajout de l'accelerometre et du vibreur

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Sensors;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Phone.Devices.Notification;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,7 +29,42 @@ namespace ShakeNTouch
         {
             this.InitializeComponent();
 
+            Viber_button.Click += Viber_button_Click;
+            accel();
             this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+        public void accel()
+        {
+            var acc = Accelerometer.GetDefault();
+            acc.ReadingChanged += Acc_ReadingChanged; ;
+            
+        }
+        public bool shake()
+        {
+
+        }
+        async private void Acc_ReadingChanged(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
+        {
+            var precx;
+            var precy;
+            var precz;
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var x = Math.Round(args.Reading.AccelerationX, 5);
+                var y = Math.Round(args.Reading.AccelerationY, 5);
+                var z = Math.Round(args.Reading.AccelerationZ, 5);
+                
+                xstate.Text = String.Format("{0,5:0.0000}", x);
+                ystate.Text = String.Format("{0,5:0.0000}", y);
+                zstate.Text = String.Format("{0,5:0.0000}", z);
+            });
+        }
+
+        private void Viber_button_Click(object sender, RoutedEventArgs e)
+        {
+            VibrationDevice testVibrationDevice = VibrationDevice.GetDefault();
+            testVibrationDevice.Vibrate(TimeSpan.FromMilliseconds(500));             
         }
 
         /// <summary>

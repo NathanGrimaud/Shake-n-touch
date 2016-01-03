c9e2559029660d8aa1e5afa41ff9638335439ed3
NathanGrimaud <grimaudnathan@gmail.com>
2016-01-03 19:24:42 +0100

    a

---
+++ ViewModel/HomePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ShakeNTouch.Model;
+using ShakeNTouch.ViewModel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Phone.Devices.Notification;
+using Windows.UI.Xaml;
+
+namespace ShakeNTouch.ViewModel
+{
+    class HomePageViewModel : ViewModelBase
+    {
+        public HomePageViewModel()
+        {
+
+        }
+    }
+}
